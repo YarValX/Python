715df26d143834986fc68ee52172d018909c49e9
YarValX <124351076+YarValX@users.noreply.github.com>
2023-05-26 10:02:57 +0500

    добавили 6 и 7 строку

--- seminar7/018.py
+++ seminar7/018.py
@@ -3,4 +3,6 @@ def print_operation_table(operation, num_rows, num_columns):
         print()
         for j in range(1,num_columns+1):
             print(operation(i,j), end="   ")
-print_operation_table(lambda x, y: x * y,6,6)
+num_rows = 3           
+num_columns = 3
+print_operation_table(lambda x, y: x * y, num_rows, num_columns )
